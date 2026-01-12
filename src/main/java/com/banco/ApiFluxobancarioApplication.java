36a24553c5c306fba018be1a19464e4148e7e9d1
Lourival Linard <l.linard@gmail.com>
2026-01-12 17:52:19 -0300

    estrutura do projeto

---
+++ src/main/java/com/banco/ApiFluxobancarioApplication.java
@@ -0,0 +1,13 @@
+package com.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFluxobancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFluxobancarioApplication.class, args);
+	}
+
+}
